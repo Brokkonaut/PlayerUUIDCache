d510004358ed02907871905ea9f788330c1e1c05
Brokkonaut <hannos17@gmx.de>
2021-06-30 07:36:23 +0200

    New database driver class

--- src/main/java/de/iani/playerUUIDCache/util/sql/MySQLConnection.java
+++ src/main/java/de/iani/playerUUIDCache/util/sql/MySQLConnection.java
@@ -4,6 +4,6 @@ import java.sql.SQLException;
 
 public class MySQLConnection extends SQLConnection {
     public MySQLConnection(String host, String database, String user, String password) throws SQLException {
-        super("jdbc:mysql://" + host + "/" + database + "?requireSSL=false&verifyServerCertificate=false", database, user, password, "com.mysql.jdbc.Driver");
+        super("jdbc:mysql://" + host + "/" + database + "?requireSSL=false&verifyServerCertificate=false", database, user, password, "com.mysql.cj.jdbc.Driver");
     }
 }
